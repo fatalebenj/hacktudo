a388e0594de22a4aa3100077678ed788d1f376e2
fatalebenj <benjmn_conz@proton.me>
2026-09-12 12:42:36 -0300

    Add tests for browser detection with various User-Agent strings
    - Created single.php to test detection of a single User-Agent string.
    - Added tv.php to test detection across multiple TV User-Agent strings.
    - Introduced webview.php for testing WebView User-Agent strings.
    - Added useragents.db file containing a large set of User-Agent strings for comprehensive testing.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a7ea031a7770ace1f9af03cc6f5f0e27a6e7119d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a7ea031a7770ace1f9af03cc6f5f0e27a6e7119d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'foroco/php-browser-detection' => array(
+            'pretty_version' => 'v2.8',
+            'version' => '2.8.0.0',
+            'reference' => '0a640254c6e61fe8e741d1e5c3495308cfec51c1',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../foroco/php-browser-detection',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
